32db448be422c76ed54229b553266416a1d593db
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-08-04 19:35:05 -0300

    Initial Commit

---
+++ Proffy/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Proffy
+//
+//  Created by Raphael Cerqueira on 04/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
